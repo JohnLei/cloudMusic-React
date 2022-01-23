9c54a3569abba1015929822ff71fd70d4a1bc868
lei <862639456@qq.com>
2022-01-23 23:07:07 +0800

    feat:新碟上架页面绘制

--- src/pages/discover/c-pages/recommend/c-cpns/new-album/index.jsx
+++ src/pages/discover/c-pages/recommend/c-cpns/new-album/index.jsx
@@ -6,7 +6,55 @@ import { getNewAlbumAction } from "../../store/actionCreators";
 import './style.less'
 
 export default memo(function HYNewAlbum() {
+  // redux-hooks
+  const { newAlbums } = useSelector((state) => ({
+    newAlbums: state.getIn(['recommend', 'newAlbums'])
+  }), shallowEqual)
+
+  const dispath = useDispatch()
+
+  useEffect(() => {
+    dispath(getNewAlbumAction(10))
+  }, [dispath])
+
+  const pageRef = useRef()
+  console.log(newAlbums);
+
   return (
-    <div>222</div>
+    <div className="fhbcbQ">
+      <HYThemeHeaderRCM title={"新碟上架"} />
+      <div className="content">
+        <button
+          className="arrow arrow-left sprite_02"
+          onClick={ (e) => pageRef.current.prev()}
+        >
+        </button>
+        <div className="album">
+          <Carousel dots={ false } ref={ pageRef }>
+            {
+              [0, 1].map((item) => {
+                return (
+                  <div className="page" key={ item }>
+                    {
+                      newAlbums.slice(item * 5, (item + 1) * 5).map((i) => {
+                        return (
+                          <div>222</div>
+                        )
+                      })
+                    }
+                  </div>
+                )
+              })
+            }
+          </Carousel>
+        </div>
+        <button
+          className="arrow arrow-right sprite_02"
+          onClick={ (e) => pageRef.current.next() }
+
+        >
+        </button>
+      </div>
+    </div>
   )
 })
